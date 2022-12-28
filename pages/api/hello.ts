79436d75bdbe76e399eb162e3162f880326cbac9
Gleb Zavalov <oldg590@gmail.com>
2022-12-28 22:25:32 +0300

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,50 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+// 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
+
 type Data = {
-  name: string
+  message: string
+  status?: string
 }
+
+const openaiApiKey = 'YOUR_API_KEY_HERE'
+const text = 'Hello, how are you today?'
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' })
+    return
+  } else {
+    try {
+      fetch('https://api.openai.com/v1/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${openaiApiKey}`,
+        },
+        body: JSON.stringify({
+          model: 'text-davinci-003',
+          prompt: text,
+          max_tokens: 100,
+          temperature: 1,
+        }), 
+      })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          // Success! Parse the response and use it in your app
+          const generatedText = responseJson.data
+          console.log(generatedText)
+        })
+    } catch (error) {
+      console.error(error)
+      res.status(405).json({
+        status: 'failure',
+        message: 'Error submitting the enquiry form',
+      })
+    }
+  }
 }
---
+++ pages/api/openaiApi.ts
@@ -0,0 +1,62 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai'
+
+type Data = {
+  message: string
+  status?: string
+}
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message:
+          'OpenAI API key not configured, please follow instructions in README.md',
+      },
+    })
+    return
+  }
+
+  const { message } = req.body || ''
+
+  if (message?.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: 'Please enter a valid message',
+      },
+    })
+    return
+  }
+
+  try {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: message,
+      max_tokens: 100,
+      temperature: 0.6,
+    })
+    res.status(200).json({ message: response.data.choices[0].text })
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data)
+      res.status(error.response.status).json(error.response.data)
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`)
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        },
+      })
+    }
+  }
+}
